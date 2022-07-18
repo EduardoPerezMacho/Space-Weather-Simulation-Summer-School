945cf984e88a2822da813c1e120b7edd18027a27
Eduardo Macho <edu_point@hotmail.com>
2022-07-18 19:19:38 -0300

    Day 1 Project

--- day_1/cos_approx.py
+++ day_1/cos_approx.py
@@ -3,17 +3,26 @@
 
 cosine approximation function
 """
-__author__ = 'Qusai Al Shidi'
-__email__ = 'qusai@umich.edu'
+__author__ = 'Eduardo Macho'
+__email__ = 'edu_point@hotmail.com'
 
 from math import factorial
 from math import pi
 
 
+def cos_terms(n, x):
+    """Divide the calculus in 3 parts"""
+    part1 = (-1)**n
+    part2 = factorial(2*n)
+    part3 = x**(2*n)
+    return part1/part2*part3    
+
+
 def cos_approx(x, accuracy=10):
+    """Calculate the cosine of a number using Taylor series
     """
-    """
-    return 
+    result = sum(cos_terms(n, x) for n in range(accuracy+1)) 
+    return result
 
 
 
